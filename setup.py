a228be01be69e02bab01124c4b60787ebe68d4fa
Arya Aftab <arya.aftab@gmail.com>
2021-09-12 09:02:58 +0430

    Added Version 0.1.1

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
+import os
+import codecs
 from setuptools import setup, find_packages
+
+
+current_path = os.path.abspath(os.path.dirname(__file__))
+
+
+def read_file(*parts):
+    with codecs.open(os.path.join(current_path, *parts), 'r', 'utf8') as reader:
+        return reader.read()
+        
+        
 
 setup(
   name = 'rotary-embedding-tensorflow',
   packages = find_packages(),
-  version = '0.1.0',
+  version = '0.1.1',
   license='MIT',
   description = 'Rotary Embedding - Tensorflow',
+  long_description=read_file('README.md'),
+  long_description_content_type='text/markdown',
   author = 'Arya Aftab',
   author_email = 'arya.aftab@gmail.com',
   url = 'https://github.com/AryaAftab/rotary-embedding-tensorflow',
@@ -16,7 +30,7 @@ setup(
   ],
   install_requires=[
     'numpy>=1.18.5',
-    'tensorflow>=2.3'
+    'tensorflow>=2.2'
   ],
   classifiers=[
     'Development Status :: 4 - Beta',
